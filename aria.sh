a167a24b628baa2251efdaa2af6d5cd03752a1f1
neilctrl <70837671+neilctrl@users.noreply.github.com>
2021-02-20 16:15:09 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,8 +1,7 @@
 export MAX_DOWNLOAD_SPEED=0
 export MAX_CONCURRENT_DOWNLOADS=10
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
-  --max-connection-per-server=16 --rpc-max-request-size=1024M \
-  --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED --bt-stop-timeout=1200 \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
-   --bt-max-peers=0 --peer-id-prefix=-TR2770- --user-agent=Transmission/2.77 --peer-agent=Transmission/2.77 \
+  --max-connection-per-server=10 --rpc-max-request-size=1024M \
+  --seed-time=0.01 --bt-stop-timeout=300 --min-split-size=10M --follow-torrent=mem --split=10 \
+   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
+   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
